9a98b2e15980ccd62e659d5425d43b1a36b45faa
Elizaveta Grishina <48994521+besperspektivnyak@users.noreply.github.com>
2020-05-05 17:40:23 +0300

    Update Spliting.py
    Добавлены функции удаления лишних символов

--- split_text/Spliting.py
+++ split_text/Spliting.py
@@ -1,15 +1,26 @@
+import nltk
 from nltk.tokenize import word_tokenize
 from nltk.stem import SnowballStemmer
 from nltk.corpus import stopwords
+import re
 
 #Разделяем текст на слова, словами также явялются метки (скобки).
 #На входе строка с именем файла. На выходе список разделенных слов.
 def split_text(file):
-    fp = open('test.txt', 'r')
+    fp = open(file, 'r', encoding='windows-1251')
     text = fp.read()
     fp.close()
+    text = del_non_words(text)
     words = word_tokenize(text, "russian")
     return words
+
+#Удаляет все 'не слова', т.е. цифры, числа, знаки препинания. На входе текст, на выходе текст без лишних символов.
+def del_non_words(text):
+    pattern = r"[0-9]"
+    pattern2 = r"[^\w]"
+    text = re.sub(pattern, " ", text)
+    text = re.sub(pattern2, " ", text)
+    return text
 
 #Разбиваем все слова на блоки из слов.
 #На входе список слов words и количество слов в одном блоке num_of_words. Пока не закончились слова, возвращает блоки.
@@ -33,7 +44,7 @@ def stemming(words):
     result = [stemmer.stem(word) for word in words]
     return result
 
-#Удалаяет стоп-слова. На входе список слов words и список дополнительных стоп-слов. На выходе список без стоп-слов.
+#Удалаяет стоп-слова. На входе список слов words и список дополнительных стоп слов. На выходе список без стоп-слов.
 def del_words(words, extra_words):
     new_words = list()
     stop_words = list(stopwords.words('russian'))
@@ -43,11 +54,13 @@ def del_words(words, extra_words):
             new_words.append(word)
     return new_words
 
+
 #Разделение текста на блоки слов, уже нормализованных стеммингом. На входе имя файла и количество слов в блоке.
 #На выходе список блоков слов заданного размера.
 def splitting(file, num_of_words):
     words = split_text(file)
-    words = del_words(words, ['.'])
+    words = del_words(words, ['Speaker'])
+    #в идеале на этой строке должна располагаться функция, отбрасывающая стоп-слова
     words = stemming(words)
     blocks_of_words = unite_in_blocks(words, num_of_words)
     return blocks_of_words
