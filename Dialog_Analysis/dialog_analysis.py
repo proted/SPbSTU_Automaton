77288e1812742d5a639696a966738062ed1eedb9
Elizaveta Grishina <48994521+besperspektivnyak@users.noreply.github.com>
2020-05-10 12:14:01 +0300

    Update dialog_analysis.py
    Добавлены комментарии.

--- Dialog_Analysis/dialog_analysis.py
+++ Dialog_Analysis/dialog_analysis.py
@@ -1,6 +1,7 @@
 from splitting import splitting
 from singular_decomposition import topics
 
-
+#Функция определения темы. На входе строка с именем файла text (str: text) и ID записи в БД ID (int: ID). 
+#Возвращает список тем (list).
 def topics_of_dialogue(text, ID):
     return topics(splitting(text, 10))
