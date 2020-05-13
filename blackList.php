74011b157fa36d033d342aa4a5f30deafd8cbb7b
Drozdova-Daria <60818854+Drozdova-Daria@users.noreply.github.com>
2020-05-13 11:12:39 +0300

    Update blackList.php

--- blackList.php
+++ blackList.php
@@ -14,13 +14,19 @@
         </div>
         <?php
         require_once 'connection.php';
-        $link = mysqli_connect($host, $user, $password, $database) or die("Ошибка подключения к базе данных" . mysqli_error($link));
-        $sql = mysqli_query($link, 
+        //$link = mysqli_connect($host, $user, $password, $database) or die("Ошибка подключения к базе данных" . mysqli_error($link));
+        $link = pg_connect($host, $user, $password, $database) or die("Ошибка подключения к базе данных" . pg_result_error($link));
+        /*$sql = mysqli_query($link, 
+        "SELECT cl.id_client, cl.phone_number
+        FROM client cl
+        WHERE cl.blacklist = 1;");*/
+        $sql = pg_query($link, 
         "SELECT cl.id_client, cl.phone_number
         FROM client cl
         WHERE cl.blacklist = 1;");
         if ($sql) {
-            $rows = mysqli_num_rows($sql);
+            //$rows = mysqli_num_rows($sql);
+            $rows = pg_num_rows($sql);
             if($rows!=0) {
                 echo "<table  border=1  cellspacing=0 cellpading=0>
                 <tread>
@@ -32,7 +38,8 @@
                 <tread>";
                 echo "<tbody>";
                 for($i=1;$i<=$rows;++$i) {
-                    $row=mysqli_fetch_row($sql);
+                    //$row=mysqli_fetch_row($sql);
+                    $row=pg_fetch_row($sql);
                     echo "<tr>";
                     for ($j=0; $j<3; ++$j) {
                         echo "<td>$row[$j]</td>";
@@ -45,9 +52,11 @@
             else {
                 echo "<div id=main><p>Черный список пуст</p></div>";
             }
-            mysqli_free_result($sql);
+            //mysqli_free_result($sql);
+            pg_free_result($sql);
         }
-        mysqli_close($link);
+        //mysqli_close($link);
+        pg_close($link);
         ?>
     </div>
   <div id="footer"> <a href="http://test1.ru/opd/index.html">Home</a> &copy;2020 </div>
