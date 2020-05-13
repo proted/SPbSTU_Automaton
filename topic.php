d30d60c1697f6cc015ada4fc6af4e7144c3cc4ce
Drozdova-Daria <60818854+Drozdova-Daria@users.noreply.github.com>
2020-05-13 11:19:12 +0300

    Update topic.php

--- topic.php
+++ topic.php
@@ -22,8 +22,16 @@
         <?php
             if(!empty($_POST["topic"])){
                 require_once 'connection.php';
-                $link = mysqli_connect($host, $user, $password, $database) or die("Ошибка подключения к базу данных" . mysqli_error($link));
-                $sql = mysqli_query($link, 
+                //$link = mysqli_connect($host, $user, $password, $database) or die("Ошибка подключения к базу данных" . mysqli_error($link));
+                $link = pg_connect($host, $user, $password, $database) or die("Ошибка подключения к базу данных" . mysqli_error($link));
+                /*$sql = mysqli_query($link, 
+                "SELECT dt.date_time_accept, inf.id_client, op.operator_team, rec.topic
+                FROM record rec 
+                LEFT JOIN information inf ON rec.id_information = inf.id_information
+                LEFT JOIN date_time dt ON dt.id_date_time = inf.id_date_time
+                LEFT JOIN operator op ON op.id_operator = inf.id_operator
+                WHERE topic = '{$_POST['topic']}'");*/
+                $sql = pg_query($link, 
                 "SELECT dt.date_time_accept, inf.id_client, op.operator_team, rec.topic
                 FROM record rec 
                 LEFT JOIN information inf ON rec.id_information = inf.id_information
@@ -32,7 +40,8 @@
                 WHERE topic = '{$_POST['topic']}'");
                 if ($sql) {
                     if($rows!=0) {
-                        $rows = mysqli_num_rows($sql);
+                        //$rows = mysqli_num_rows($sql);
+                        $rows = pg_num_rows($sql);
                         echo "<table  border=1  cellspacing=0 cellpading=0>
                         <tread>
                         <tr>
@@ -44,7 +53,8 @@
                         <tread>";
                         echo "<tbody>";
                         for($i=1;$i<=$rows;++$i) {
-                            $row=mysqli_fetch_row($sql);
+                            //$row=mysqli_fetch_row($sql);
+                            $row=pg_fetch_row($sql);
                             echo "<tr>";
                             for ($j=0; $j<4; ++$j) {
                                 echo "<td>$row[$j]</td>";
@@ -57,9 +67,11 @@
                     else {
                          echo "<div id=main><p>Записей с данной темой не найдено</p></div>";
                     }
-                    mysqli_free_result($sql);
+                    //mysqli_free_result($sql);
+                    $row=pg_fetch_row($sql);
                 }
-                mysqli_close($link);
+                //mysqli_close($link);
+                pg_close($link);
             }
         ?>
     </div>
