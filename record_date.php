0dd4962e8bea1b55b5a5aaaccf9ea614536a4218
Drozdova-Daria <60818854+Drozdova-Daria@users.noreply.github.com>
2020-05-14 15:15:13 +0300

    Исправлен запрос к базе данных

--- record_date.php
+++ record_date.php
@@ -22,32 +22,38 @@
         </div>
         <?php
             if(!empty($_POST["date_start"]) & !empty($_POST["date_end"])){
-                require_once 'connection.php';
-                //$link = mysqli_connect($host, $user, $password, $database) or die("Ошибка подключения к базу данных" . mysqli_error($link));
-                $link = pg_connect($host, $user, $password, $database) or die("Ошибка подключения к базу данных" . pg_result_error($link));
+                //require_once 'connection.php';
+                //$link = mysqli_connect($host, $user, $password, $database) or die("Ошибка подключения к базе данных" . mysqli_error($link));
+                $link = pg_connect($host, $user, $password, $database) or die("Ошибка подключения к базе данных" . pg_result_error($link));
                 /*$sql = mysqli_query($link, 
-                "SELECT dt.date_time_accept, inf.id_client, op.operator_team, rec.topic
+                "SELECT dt.date_time_accept, cl.phone_number, op.operator_team, top.name
                 FROM date_time dt 
-                LEFT JOIN information inf ON dt.id_date_time = inf.id_information 
+                LEFT JOIN information inf ON dt.id_date_time = inf.id_date_time
                 LEFT JOIN operator op ON op.id_operator = inf.id_operator
                 LEFT JOIN record rec ON rec.id_information = inf.id_information
-                WHERE date_time_accept > '{$_POST['date_start']}' AND date_time_accept < '{$_POST['date_end']}';");*/
+                LEFT JOIN record_topic rec_t ON rec_t.id_record=rec.id_record
+                LEFT JOIN topic top ON top.id_topic=rec_t.id_topic
+                LEFT JOIN client cl ON cl.id_client=inf.id_client
+                WHERE date_time_accept > date '{$_POST['date_start']}' AND date_time_accept < date '{$_POST['date_end']}';");*/
                 $sql = pg_query($link, 
-                "SELECT dt.date_time_accept, inf.id_client, op.operator_team, rec.topic
+                "SELECT dt.date_time_accept, cl.phone_number, op.operator_team, top.name
                 FROM date_time dt 
-                LEFT JOIN information inf ON dt.id_date_time = inf.id_information 
+                LEFT JOIN information inf ON dt.id_date_time = inf.id_date_time
                 LEFT JOIN operator op ON op.id_operator = inf.id_operator
                 LEFT JOIN record rec ON rec.id_information = inf.id_information
+                LEFT JOIN record_topic rec_t ON rec_t.id_record=rec.id_record
+                LEFT JOIN topic top ON top.id_topic=rec_t.id_topic
+                LEFT JOIN client cl ON cl.id_client=inf.id_client
                 WHERE date_time_accept > '{$_POST['date_start']}' AND date_time_accept < '{$_POST['date_end']}';");
                 if ($sql) {
+                    //$rows = mysqli_num_rows($sql);
+                    $rows=pg_num_rows($sql);
                     if($rows!=0) {
-                        //$rows = mysqli_num_rows($sql);
-                        $rows=pg_num_rows($sql);
                         echo "<table  border=1  cellspacing=0 cellpading=0>
                         <tread>
                         <tr>
                         <td>Дата принятия вызова</sup></td>
-                        <td>Id клиента</td>
+                        <td>Номер клиента</td>
                         <td>Команда оператора</td>
                         <td>Тема разговора</td>
                         </tr>
