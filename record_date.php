2eefe7bf925c76a01153e64fea4d2e979d8b438e
Drozdova-Daria <60818854+Drozdova-Daria@users.noreply.github.com>
2020-05-15 22:10:28 +0300

    Исправлен запрос к бд (выбор темы)

--- record_date.php
+++ record_date.php
@@ -17,34 +17,36 @@
                 <p>Введите период:</p>
                 <input type="datetime-local" id="localdate" name="date_start"/> -
                 <input type="datetime-local" id="localdate" name="date_end"/>
-                <p><input type="submit" value="Enter"></p>
+                <p><input type="submit" value="Enter"/></p>
             </form>
         </div>
         <?php
             if(!empty($_POST["date_start"]) & !empty($_POST["date_end"])){
-                //require_once 'connection.php';
+                require_once 'connection.php';
                 //$link = mysqli_connect($host, $user, $password, $database) or die("Ошибка подключения к базе данных" . mysqli_error($link));
                 $link = pg_connect($host, $user, $password, $database) or die("Ошибка подключения к базе данных" . pg_result_error($link));
                 /*$sql = mysqli_query($link, 
-                "SELECT dt.date_time_accept, cl.phone_number, op.operator_team, top.name
+                "SELECT rec.id_record, dt.date_time_accept, cl.phone_number, t.name
                 FROM date_time dt 
                 LEFT JOIN information inf ON dt.id_date_time = inf.id_date_time
                 LEFT JOIN operator op ON op.id_operator = inf.id_operator
+                LEFT JOIN team t ON t.id_team=op.id_team
                 LEFT JOIN record rec ON rec.id_information = inf.id_information
                 LEFT JOIN record_topic rec_t ON rec_t.id_record=rec.id_record
                 LEFT JOIN topic top ON top.id_topic=rec_t.id_topic
                 LEFT JOIN client cl ON cl.id_client=inf.id_client
                 WHERE date_time_accept > date '{$_POST['date_start']}' AND date_time_accept < date '{$_POST['date_end']}';");*/
-                $sql = pg_query($link, 
-                "SELECT dt.date_time_accept, cl.phone_number, op.operator_team, top.name
+                $sql = mysqli_query($link, 
+                "SELECT rec.id_record, dt.date_time_accept, cl.phone_number, t.name
                 FROM date_time dt 
                 LEFT JOIN information inf ON dt.id_date_time = inf.id_date_time
                 LEFT JOIN operator op ON op.id_operator = inf.id_operator
+                LEFT JOIN team t ON t.id_team=op.id_team
                 LEFT JOIN record rec ON rec.id_information = inf.id_information
                 LEFT JOIN record_topic rec_t ON rec_t.id_record=rec.id_record
                 LEFT JOIN topic top ON top.id_topic=rec_t.id_topic
                 LEFT JOIN client cl ON cl.id_client=inf.id_client
-                WHERE date_time_accept > '{$_POST['date_start']}' AND date_time_accept < '{$_POST['date_end']}';");
+                WHERE date_time_accept > date '{$_POST['date_start']}' AND date_time_accept < date '{$_POST['date_end']}';");
                 if ($sql) {
                     //$rows = mysqli_num_rows($sql);
                     $rows=pg_num_rows($sql);
@@ -59,14 +61,34 @@
                         </tr>
                         <tread>";
                         echo "<tbody>";
+                        $k=0;
+                        $id=0;
                         for($i=1;$i<=$rows;++$i) {
                             //$row=mysqli_fetch_row($sql);
                             $row=pg_fetch_row($sql);
-                            echo "<tr>";
-                            for ($j=0; $j<4; ++$j) {
-                                echo "<td>$row[$j]</td>";
+                            if($row[0]!=$id){
+                                $flag=0;
                             }
-                            echo "</tr>";
+                            if($flag==0) {
+                                $flag=1;
+                                echo "<tr>";
+                                for ($j=1; $j<4; ++$j) {
+                                    echo "<td>$row[$j]</td>";
+                                }
+                                $id=$row[0];
+                                //$sql1=mysqli_query($link, "SELECT t.name FROM topic t LEFT JOIN record_topic rec_top ON rec_top.id_topic=t.id_topic  WHERE rec_top.id_record='$row[0]';");
+                                $sql1=pg_query($link, "SELECT t.name FROM topic t LEFT JOIN record_topic rec_top ON rec_top.id_topic=t.id_topic  WHERE rec_top.id_record='$row[0]';");
+                                //$rows1 = mysqli_num_rows($sql1);
+                                $rows1 = pg_num_rows($sql1);
+                                echo "<td>";
+                                for($k=1;$k<=$rows1;$k++) {
+                                    //$row=mysqli_fetch_row($sql1);
+                                    $row=pg_fetch_row($sql1);
+                                    echo "$row[0], ";
+                                }
+                                echo "</td>";
+                                echo "</tr>"; 
+                            }
                         }
                         echo "</tbody>";
                         echo "</table>";
