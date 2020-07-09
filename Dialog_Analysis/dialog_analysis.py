04b05d0a7ddbc5d56195009178fc91ad1b6e0458
Elizaveta Grishina <48994521+besperspektivnyak@users.noreply.github.com>
2020-07-09 22:22:59 +0300

    Исправлена ошибка в коде
    ошибка невнимательного программиста, перепутавшего функции

--- Dialog_Analysis/dialog_analysis.py
+++ Dialog_Analysis/dialog_analysis.py
@@ -1,7 +1,7 @@
 from splitting import splitting
 from singular_decomposition import topics
 from assessment import number
-from assessment import create_log
+from assessment import assessment
 
 
 
@@ -11,5 +11,8 @@ def analysis(text, id):
     :param id:str  -  id разговора
     :return: tuple - оценка, лог, темы
     """
-    result = number(), create_log(text, id), topics(splitting(text, 10))
+    result = number(), assessment(text, id), topics(splitting(text, 10))
     return result
+
+
+print(analysis("testbad.txt", "rje"))
