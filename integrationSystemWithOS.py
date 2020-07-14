996e6b15a445dd5a1d936d355e2c88d2476f3a43
OvechkinDanil <47829271+OvechkinDanil@users.noreply.github.com>
2020-07-14 15:01:19 +0300

    Update integrationSystemWithOS.py
    Добавил бесконечный цикл и заглушку для функции распознаванаия

--- integrationSystemWithOS.py
+++ integrationSystemWithOS.py
@@ -1,21 +1,40 @@
 #Модуль os для работы с операционной системой
 import os
+#Директория необработанных файлов txt
+UnprDirTxt = "D:\\TxtFiles\\unprocessed\\"
+#Директория обработанных файлов txt
+PrDirTxt = "D:\\TxtFiles\\processed\\"
+#Директория обработанных файлов mp3
+UnprDirMp3 = "D:\\mp3Files\\unprocessed\\"
+#Директория обработанных файлов mp3
+PrDirMp3 = "D:\\mp3Files\\processed\\"
 
-#Директория необработанных файлов
-UnprDir = "D:\\TxtFiles\\unprocessed"
-
-#Директория обработанных файлов
-PrDir = "D:\\TxtFiles\\processed\\"
-
-#Выбор директории, где лежат необработанные файлы
-os.chdir(UnprDir)
 
 #Цикл по всем элементам в этой папке
-while(len(os.listdir()) != 0):
-     #Выбор последнего файла в папке
-     curFile = os.listdir().pop()
-     #Запуск системы оценивания
-     #TODO: добавить вызов функции для оценивания
+while True:
+     # Выбор директории, где лежат необработанные mp3 файлы
+     os.chdir(UnprDirMp3)
+     if len(os.listdir()) != 0:
+          #Выбор последнего файла в папке c mp3 файлами
+          curFileMp3 = os.listdir().pop()
+          #Запуск системы оценивания
+          #TODO: добавить вызов функции для распознавания
 
-     #Перенос файла из папки "необработанные" в "обработанные"
-     os.replace(curFile, PrDir + curFile)
+          #Перенос файла из папки "необработанные" в "обработанные"
+          os.replace(curFileMp3, PrDirMp3 + curFileMp3)
+
+          # Выбор директории, где лежат необработанные txt файлы
+     os.chdir(UnprDirTxt)
+     if len(os.listdir()) != 0:
+          # Выбор последнего файла в папке с txt файлами
+          curFileTxt = os.listdir().pop()
+          # Запуск системы оценивания
+          # TODO: добавить вызов функции для оценивания
+
+          # Перенос файла из папки "необработанные" в "обработанные"
+          os.replace(curFileTxt, PrDirTxt + curFileTxt)
+
+
+
+
+
